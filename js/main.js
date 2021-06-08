4bb69e4b36cce278d1a20f849c59cf696cbd2ea6
nmorajda <n.morajda@abmstudio.pl>
2021-06-08 10:00:00 +0200

    Remove inline script and add main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const btnsContainer = document.querySelector('.buttons');
+const textContainer = document.getElementById('text');
+
+btnsContainer.addEventListener('click', e => {
+  const btns = btnsContainer.querySelectorAll('[data-font-size]');
+  if (e.target.classList.contains('btn')) {
+    [...btns].forEach(btn => btn.classList.remove('active'));
+    e.target.classList.add('active');
+    textContainer.style.fontSize = `${e.target.dataset.fontSize}em`;
+  }
+});
